9ec64921625ae54742d6597ca028c31e34fd33aa
vijaychauhan54 <vijauchauhan88257@gmail.com>
2026-08-12 00:59:26 +0530

    Initial commit - SiteMaker API

---
+++ src/main/java/com/springrest/springrest/SiteMakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteMakerApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springrest/springrest/SiteMakerController.java
@@ -0,0 +1,34 @@
+package com.springrest.springrest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.model.Login;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+
+@RestController
+public class SiteMakerController {
+
+@Value("${payusalt}")
+private String payusalt;
+	
+	@PostMapping("/login")
+	public String login(HttpServletRequest request,@RequestBody Login login) {
+		 
+		
+		System.out.println("Authcode :-- ");
+		
+		System.out.println("API:--: "+payusalt);
+		
+		return "OUR API PROJECT CALL";
+	}
+	
+	
+	
+}
